9012ba51129eee20ea35da4da395a411c8fa147b
Ruslan Gureev <ruslanus1989@gmail.com>
2022-10-04 18:11:48 +0300

    Reshenie zadachi

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -1,3 +1,26 @@
 ﻿Console.WriteLine("Задан массив строк: ");
 string[] Array = new string[5] { "Zadanie", "Son", "33", ":)", "1989" };
-Console.WriteLine(string.Join(", ", Array));
+int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"{Array[i]},");
+    if (Array[i].Length < 4)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("Готовый массив:");
+
+string[] Array2 = new string[count];
+int j = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length < 4)
+    {
+        Array2[j] = Array[i];
+        Console.Write($"{Array2[j]},");
+        j++;
+    }
+}
